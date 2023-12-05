533954f0de0eb99744bdb737a509c4e3fa0b7a31
Abudhalsha <aabudhalsha142155@gmail.com>
2023-12-04 22:55:16 -0500

    first commit

---
+++ React/src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import './App.css'
+import PriceCard from "./PriceCard"
+
+function App() {
+ 
+
+  return (
+    <div>
+    <PriceCard></PriceCard>
+    </div>
+
+    
+  )
+}
+
+export default App
